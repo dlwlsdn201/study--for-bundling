dc6498cacedba5a7839e4d129e4610126df7a0c4
JinWoo_Lee <dlwlsdn201@naver.com>
2025-12-18 21:17:33 +0900

    🔩 `webpack.config.js` 기초 설정들 정의 && html 에 번들 파일 스크립트 연동

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  /**
+   * @desc mode
+   * 'development' : 소스맵 기본 포함, 최소한의 압축(공백제거, 변수명 단순화 등) -> 디버깅 쉬운 환경 제공
+   * 'production' : 최대한 압축(코드 압축, tree shaking 등) 고급 최적화 -> 파일 크기 최소화에 집중
+   * 'none' : 최적화 없음, 소스맵 제외 -> 모든 설정 커스터마이징 때 사용
+   */
+  mode: "development",
+  entry: "./main.js", // 어떤 파일을 진입점으로 번들링할지
+  output: {
+    filename: "bundle.js", // 번들로 만들어질 파일 이름
+    path: path.resolve(__dirname, "dist"), // 번들 파일이 저장될 위치
+  },
+};
