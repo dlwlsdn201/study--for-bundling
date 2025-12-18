62ed5d9d165828cb047ef8c2fe8f69f7bd6d773b
JinWoo_Lee <dlwlsdn201@naver.com>
2025-12-18 22:03:54 +0900

    :zap: emoji 전역 변수를 모듈로 변환

--- emoji.js
+++ emoji.js
@@ -1,8 +1,11 @@
-const emojis = [
-  { icon: '😊', name: 'Smiling Face' },
-  { icon: '🚀', name: 'Rocket' },
-  { icon: '🍕', name: 'Pizza' },
-  { icon: '🐱', name: 'Cat' },
-  { icon: '🌈', name: 'Rainbow' },
-  { icon: '🎸', name: 'Guitar' }
+// 'export' 키워드를 붙이면 -> "모듈화"
+// 'export' 키워드를 붙이지 않으면 -> "전역 변수/함수"
+
+export const emojis = [
+  { icon: "😊", name: "Smiling Face" },
+  { icon: "🚀", name: "Rocket" },
+  { icon: "🍕", name: "Pizza" },
+  { icon: "🐱", name: "Cat" },
+  { icon: "🌈", name: "Rainbow" },
+  { icon: "🎸", name: "Guitar" },
 ];
