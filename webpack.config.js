e23a1770262f2611a1a5fcc3395e1e2b387c9375
JinWoo_Lee <dlwlsdn201@naver.com>
2026-01-14 22:11:47 +0900

    🔩 `react` 기반으로 모듈 파일 확장자 및 내부 코드 문법 수정 && react build 환경 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
    * 'none' : 최적화 없음, 소스맵 제외 -> 모든 설정 커스터마이징 때 사용
    */
   mode: "development",
-  entry: "./main.ts", // 어떤 파일을 진입점으로 번들링할지
+  entry: "./main.tsx", // 어떤 파일을 진입점으로 번들링할지
   output: {
     filename: "bundle.js", // 번들로 만들어질 파일 이름
     path: path.resolve(__dirname, "dist"), // 번들 파일이 저장될 위치
@@ -16,8 +16,25 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/, // .ts 또는 .tsx 파일들은
-        use: "ts-loader", // ts-loader를 사용하여 타입스크립트 파일을 자바스크립트로 변환
+        test: /\.(ts|tsx)$/, // .ts 또는 .tsx 파일들은 모두 처리
+        use: [
+          {
+            loader: "babel-loader", // babel-loader를 사용하여 TSX 파일을 JSX로 변환
+            options: {
+              presets: [
+                "@babel/preset-env", // 최신 JS 문법이 구 브라우저에서도 작동하게 변환해줌
+                [
+                  "@babel/preset-react", // 바벨이 react 문법(JSX)을 어떤 식으로 변환할지 알려주는 설정
+                  {
+                    runtime: "automatic", // 자동 JSX 변환 활성화
+                  },
+                ],
+                "@babel/preset-typescript", // 바벨이 typescript 문법을 어떤 식으로 변환할지 알려주는 설정
+              ],
+            },
+          },
+        ],
+        // use: "ts-loader", // ts-loader를 사용하여 타입스크립트 파일을 자바스크립트로 변환
         exclude: /node_modules/, // node_modules 폴더는 제외
       },
     ],
